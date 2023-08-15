0824164dc97d03ff7a0a39bb896b1c5f625eb953
Pap <renchen.dev@gmail.com>
2023-08-16 00:20:08 +0800

    hystrix dashboard错误

---
+++ eureka-dashboard/src/main/java/top/youngfriends/HystrixDashBoardApplication.java
@@ -0,0 +1,17 @@
+package top.youngfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author pap
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashBoardApplication.class, args);
+    }
+}
