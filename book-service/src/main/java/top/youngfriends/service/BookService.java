d84df7bcad9af9ae6dc84fe4bde4e83048756260
Pap <renchen.dev@gmail.com>
2023-08-16 17:19:00 +0800

    增加配置全局管理

--- book-service/src/main/java/top/youngfriends/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package top.youngfriends.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import top.youngfriends.mapper.BookMapper;
-import top.youngfriends.po.Book;
-
-public interface BookService {
-    Book getBookByBid(int bid);
-}
